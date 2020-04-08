9aa61e2d1d87ef1c4f06ff1851f8542a1eb2c2cf
Jakub Plona <plonajakub@gmail.com>
2020-04-09 00:43:22 +0200

    lab2: task4: add sources

---
+++ lab2/public-html/js/animals.js
@@ -0,0 +1,16 @@
+window.onload = () => {
+    const imgLabels = document.getElementsByClassName("menu-item");
+    const currentAnimalImage = document.getElementById("curr-animal-img");
+    const imgPath = "img/zad3/";
+    const imgExt = ".gif";
+    for (const imgLabel of imgLabels) {
+        imgLabel.addEventListener("mouseover", () => {
+           imgLabel.src = imgPath + imgLabel.id + "over" + imgExt;
+           currentAnimalImage.src = imgPath + imgLabel.id + imgExt;
+        });
+        imgLabel.addEventListener("mouseout", () => {
+            imgLabel.src = imgPath + imgLabel.id + "out" + imgExt;
+            currentAnimalImage.src = "img/white.svg";
+        });
+    }
+}
