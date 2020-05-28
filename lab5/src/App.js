052903c384869bdfbf278e7d3770ec97934f1d6b
Jakub Plona <plonajakub@gmail.com>
2020-05-28 19:27:48 +0200

    lab5: add router

--- lab5/src/App.js
+++ lab5/src/App.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import "./App.css"
 import WelcomePage from "./welcome-page/welcome";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useRouteMatch,
+    useParams
+} from "react-router-dom";
+
 
 function App() {
     return (
-        <WelcomePage title="MuzykaFilmowa.org"/>
+        <Router>
+            <div>
+                <div id="index">
+                    <div>Spis treści:</div>
+                    <ol>
+                        <li>
+                            <Link to="/zad1">Strona powitalna, komponenty i właściwości</Link>
+                        </li>
+                        <li>
+                            <Link to="/zad2">Przyciski, formularze i zdarzenia</Link>
+                        </li>
+                        <li>
+                            <Link to="/zad3">Komponent listy</Link>
+                        </li>
+                    </ol>
+                </div>
+
+                <Switch>
+                    <Route path="/zad1">
+                        <WelcomePage title="MuzykaFilmowa.org"/>
+                    </Route>
+                    <Route path="/zad2">
+
+                    </Route>
+                    <Route path="/zad3">
+
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     );
 }
 
