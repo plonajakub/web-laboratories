5d577a6c1aa18eabd6e74728a66832e7aeed890f
Jakub Plona <plonajakub@gmail.com>
2020-06-13 22:42:41 +0200

    lab6: prj2: ex3, ex4: add source

--- lab6/prj2/src/index.js
+++ lab6/prj2/src/index.js
@@ -1,3 +1,4 @@
 import main from './js/main.js';
+import './style/style.scss';
 
 console.log('src/index.js');
--- lab6/prj2/webpack.config.js
+++ lab6/prj2/webpack.config.js
@@ -1,9 +1,39 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist/js'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html'),
+      filename: path.resolve(__dirname, 'dist/index.html')
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/subpage.html'),
+      filename: path.resolve(__dirname, 'dist/subpage.html')
+    }),
+    new MiniCssExtractPlugin({
+      filename: '../style/style.css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Extracts CSS into separate file
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
   },
 };
