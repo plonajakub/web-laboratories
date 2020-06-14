437664ef47a58b2a0e4a8a8489e37a7e851a769a
Jakub Plona <plonajakub@gmail.com>
2020-06-14 02:34:40 +0200

    lab6: prj2: add docker setup

--- lab6/prj2/webpack.config.js
+++ lab6/prj2/webpack.config.js
@@ -5,8 +5,8 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist/js'),
+    filename: 'js/main.js',
+    path: path.resolve(__dirname, 'dist'),
     publicPath: '/'
   },
   plugins: [
